07a013db6f0d6ddf068025a89650fc626e65ff68
eng0waleed <eng.0waleed@gmail.com>
2024-03-30 21:36:04 +0300

    delete non used

--- citra_event_manager/event_manager/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('register_camera/', views.register_camera, name='register_camera'),
-    path('register_server/', views.register_server, name='register_server'),
-    # ...
-]
